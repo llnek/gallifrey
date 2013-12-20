d18cfa11f4a8d43571abb3f2a6f3163fabbcc510
Ken Leung <mailtokenl@gmail.com>
2013-12-19 17:41:11 -0800

    kl

---
+++ etc/weblibs/impactjs/tools/dummy.js
@@ -0,0 +1,27 @@
+
+ig.module('game.dummy').requires(
+
+'impact.animation',
+'impact.background-map',
+'impact.collision-map',
+'impact.entity',
+'impact.entity-pool',
+'impact.font',
+'impact.game',
+'impact.image',
+'impact.impact',
+'impact.input',
+'impact.loader',
+'impact.map',
+'impact.sound',
+'impact.system',
+'impact.timer'
+//'impact.debug.debug',
+//'impact.debug.entities-panel',
+//'impact.debug.graph-panel',
+//'impact.debug.maps-panel',
+//'impact.debug.menu'
+
+).defines(function() {
+  console.log('heeloo');
+});
